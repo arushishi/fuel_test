d1fab48404dd6559abd42eac5d8c80169b3bb44d
e_katata <ekatata@miraholdings.co.jp>
2015-03-16 19:21:43 +0900

    FuelPHP̐ݒè

--- fuel/app/bootstrap.php
+++ fuel/app/bootstrap.php
@@ -2,6 +2,8 @@
 // Bootstrap the framework DO NOT edit this
 require COREPATH.'bootstrap.php';
 
+// set default charset
+ini_set('default_charset', 'UTF-8');
 
 Autoloader::add_classes(array(
 	// Add classes you want to override here
